17633879f74c3896a451302042fb501f46791c1a
haileyng02 <haileynguyenss@gmail.com>
2023-04-14 18:27:19 +0700

    add destination content

---
+++ src/components/ToursContainer.jsx
@@ -0,0 +1,16 @@
+import TourItem from "./TourItem";
+
+const ToursContainer = ({ tours, className }) => {
+  return (
+    <div
+      className={`grid gap-y-7 w-full place-content-between ${className}`}
+      style={{ gridTemplateColumns: "repeat(3, minmax(0, 30%))" }}
+    >
+      {tours.map((tour, i) => (
+        <TourItem key={i} {...tour} />
+      ))}
+    </div>
+  );
+};
+
+export default ToursContainer;
--- src/pages/Destination.jsx
+++ src/pages/Destination.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import { Pagination } from 'antd';
+import ToursContainer from '../components/ToursContainer';
+import tours from '../constants/tours';
 
 const Destination = () => {
   return (
-    <div>Destination</div>
+    <div className='pb-[45px]'>
+      <ToursContainer tours={tours} className={'mt-[25px]'}/>
+      <Pagination defaultCurrent={1} total={50} className='mt-[47px] w-fit mx-auto'/>
+    </div>
   )
 }
 
