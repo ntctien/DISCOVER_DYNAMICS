dca5668366c2a4a2d8ea80f67be77040e40156ac
chunkin02 <trungkieenn0202@gmail.com>
2023-06-10 22:57:33 +0700

    changPassword

--- src/components/modals/ChangePassword.jsx
+++ src/components/modals/ChangePassword.jsx
@@ -1,14 +1,51 @@
-import { Modal, Button } from "antd";
+import { Modal, Button, Spin, message } from "antd";
 import logo from '../../assets/logo.png';
 import lockIcon from '../../assets/lock.svg';
+import loadingIcon from '../../assets/loading.gif';
+import React, { useState } from 'react';
+import { getAuth, updatePassword } from 'firebase/auth';
 
-const ChangePassword = ({open,handleOk , handleCancel}) => {
+const ChangePassword = ({open, handleCancel}) => {
+    const [currentPassword, setCurrentPassword] = useState('');
+    const [newPassword, setNewPassword] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    const onCancel = () => {
+        handleCancel();
+    }
+
+    const changePassword = (e) => {
+        setLoading(true);
+        e.preventDefault();
+        
+        const auth = getAuth();
+        const user = auth.currentUser;
+
+        if (user) {
+            updatePassword(user, newPassword)
+            .then(() => {
+                // Đổi mật khẩu thành công
+                message.success('Đổi mật khẩu thành công');
+                onCancel();
+            }).catch((error) => {
+                // Xử lý lỗi khi đổi mật khẩu
+                message.error('Lỗi khi đổi mật khẩu:', error);
+            });
+        }
+        setLoading(false);
+    };
 
     return (
         <>
         <Modal centered open={open} onCancel={handleCancel} footer={null}>
           <div style={{position:"relative",background:"#FFFFFF",borderRadius:"15px", padding:"24px",
             display:"flex", flexDirection:"column", justifyContent:"center", alignItems:"center"}}>
+                <Spin 
+                    spinning={loading}
+                    indicator={
+                        <img src={loadingIcon} alt="Loading" />
+                    }
+                />
                 <img
                     src={logo}
                     alt="logo"
@@ -22,28 +59,44 @@ const ChangePassword = ({open,handleOk , handleCancel}) => {
                     Nhập mật khẩu cũ: </div>
 
                 <div style={{display:"flex", flexDirection:"row", justifyContent:"flex-start", alignItems:"center", 
-                    border:"1px solid rgba(0, 0, 0, 0.47)", borderRadius:"10px", marginTop :"8px", padding:"12px", width :"470px"}}>
+                        border:"1px solid rgba(0, 0, 0, 0.47)", borderRadius:"10px", marginTop :"8px", padding:"12px", width :"470px"}}>
                         <img
                             src={lockIcon} 
                             alt="Password"/>
                         <input 
+                            value={currentPassword}
+                            onChange={(e) => setCurrentPassword(e.target.value)}
                             type="password" 
                             placeholder="Nhập mật khẩu" 
-                            style={{marginLeft :"12px",borderWidth :'0', outline:"none", width : "400px"}}></input>
+                            style={{
+                                marginLeft :"12px",
+                                borderWidth :'0', 
+                                outline:"none", 
+                                width : "400px"
+                            }}>
+                        </input>
                 </div>
 
                 <div style={{ fontSize: "20px", marginTop : "12px"}}>
                     Nhập mật khẩu mới: </div>
 
                 <div style={{display:"flex", flexDirection:"row", justifyContent:"flex-start", alignItems:"center", 
-                    border:"1px solid rgba(0, 0, 0, 0.47)", borderRadius:"10px", marginTop :"8px", padding:"12px", width :"470px"}}>
+                        border:"1px solid rgba(0, 0, 0, 0.47)", borderRadius:"10px", marginTop :"8px", padding:"12px", width :"470px"}}>
                         <img
                             src={lockIcon} 
                             alt="Password"/>
                         <input 
+                            value={newPassword}
+                            onChange={(e) => setNewPassword(e.target.value)}
                             type="password" 
                             placeholder="Nhập mật khẩu" 
-                            style={{marginLeft :"12px",borderWidth :'0', outline:"none", width : "400px"}}></input>
+                            style={{
+                                marginLeft :"12px",
+                                borderWidth :'0', 
+                                outline:"none", 
+                                width : "400px"
+                            }}>
+                        </input>
 
                 </div>
 
@@ -51,14 +104,22 @@ const ChangePassword = ({open,handleOk , handleCancel}) => {
                     Nhập lại mật khẩu: </div>
 
                 <div style={{display:"flex", flexDirection:"row", justifyContent:"flex-start", alignItems:"center", 
-                    border:"1px solid rgba(0, 0, 0, 0.47)", borderRadius:"10px", marginTop :"8px", padding:"12px", width :"470px"}}>
+                        border:"1px solid rgba(0, 0, 0, 0.47)", borderRadius:"10px", marginTop :"8px", padding:"12px", width :"470px"}}>
                         <img
                             src={lockIcon} 
                             alt="Password"/>
                         <input 
+                            value={newPassword}
+                            onChange={(e) => setNewPassword(e.target.value)}
                             type="password" 
                             placeholder="Nhập mật khẩu" 
-                            style={{marginLeft :"12px",borderWidth :'0', outline:"none", width : "400px"}}></input>
+                            style={{
+                                marginLeft :"12px",
+                                borderWidth :'0', 
+                                outline:"none", 
+                                width : "400px"
+                            }}>
+                        </input>
 
                 </div>
           </div>
@@ -68,7 +129,7 @@ const ChangePassword = ({open,handleOk , handleCancel}) => {
                     Hủy
             </Button>
 
-            <Button onClick={handleOk} 
+            <Button onClick={changePassword} 
                 style={{background: "#FF9648", color: "#FFFFFF", fontSize: "20px", height : "50px", width : "230px", 
                     marginLeft : "20px"}}>
                     Xác nhận
